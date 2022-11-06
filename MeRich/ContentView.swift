619dedbe171977fded643a052ab839b479a1042f
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-06 20:15:37 +0900

    Initial Commit

---
+++ MeRich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeRich
+//
+//  Created by Eldor Alikuvatov on 2022/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeRich/MeRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeRichApp.swift
+//  MeRich
+//
+//  Created by Eldor Alikuvatov on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct MeRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
