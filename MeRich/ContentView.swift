acbb85ea03600c0b30fd81f9c77a05246834f5a0
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-06 21:04:56 +0900

    I am Rich - SwiftUI Application

--- MeRich/ContentView.swift
+++ MeRich/ContentView.swift
@@ -2,15 +2,29 @@
 //  ContentView.swift
 //  MeRich
 //
-//  Created by Eldor Alikuvatov on 2022/11/06.
+//  Created by Eldor Alikuvvatov on 2022/11/06.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300, alignment: .center)
+            }
+        }
+        
+        
     }
 }
 
